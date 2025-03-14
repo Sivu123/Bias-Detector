a313156ce6b1dc762407dbe04d947be7b76c07d2
Sivu123 <sivu04@gmail.com>
2025-03-13 17:23:03 -0700

    Update rag.py

---
+++ rag.py
@@ -0,0 +1,40 @@
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+from langchain_openai import ChatOpenAI
+from pydantic import BaseModel, Field
+from langgraph.graph import MessagesState
+from typing import  Annotated
+import operator
+from openai import OpenAI
+from langgraph.graph import START, END, StateGraph
+from langchain_core.runnables import RunnableConfig
+from langgraph.checkpoint.memory import MemorySaver
+from langchain_community.tools.tavily_search import TavilySearchResults
+import base64
+import streamlit as st
+import os
+
+import prompts as pt
+
+os.environ["LANGCHAIN_TRACING_V2"] = st.secrets["LANGCHAIN_TRACING_V2"]
+os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGCHAIN_API_KEY"]
+os.environ["LANGCHAIN_ENDPOINT"] = st.secrets["LANGCHAIN_ENDPOINT"]
+os.environ["LANGCHAIN_PROJECT"] = st.secrets["LANGCHAIN_PROJECT"]
+os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+os.environ["TAVILY_API_KEY"] = st.secrets["TAVILY_API_KEY"]
+
+MAX_RESULTS = 5
+
+# set the openai model
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+# create client
+client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+# tavily search
+tavily_search = TavilySearchResults(max_results = MAX_RESULTS)
+class BotState(MessagesState):
+    context: list[str]
+    answers: str
+
+
+def search_web(state:BotState):
+  hot_topic = state["messages"]
+  search_docs = tavily_search.invoke(pt.
