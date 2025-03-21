d5f433b59e81439a3e41be1d814bda881442bc3b
Sivu123 <sivu04@gmail.com>
2025-03-20 17:48:48 -0700

    Imported graph stream from rag.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+from rag import graph_streamer
 image_address = "https://bs-uploads.toptal.io/blackfish-uploads/components/open_graph_image/8957316/og_image/optimized/0919_Machines_and_Trust_Lina_Social-ac9acf8ebc252ec57a9a9014f6be62b2.png"
 st.title("Bias Reader")
 st.image(image_address)
